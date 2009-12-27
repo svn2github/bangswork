3990147e7fca4bbbf48a2cc97cf15090b6802e79
bang590 <bang590@9944b59e-f2dc-11de-8b01-fd14ac940ccb>
2009-12-27 11:46:01 +0000

    git-svn-id: http://bangswork.googlecode.com/svn@2 9944b59e-f2dc-11de-8b01-fd14ac940ccb

---
+++ trunk/ndzk/collect.php
@@ -0,0 +1,61 @@
+﻿<?php
+require("simple_html_dom.php");
+if (isset($_GET['n'])) {
+	$num = (int) $_GET['n'];
+} else {
+	$num = 0;
+}
+$html = file_get_html ( $_GET['url'] );
+
+$imgs = $html->find("#MyDataList img");
+$imgsUrl = array();
+foreach ( $imgs as $img ) {
+	array_push($imgsUrl, 'http://www.nbweekly.com/' . $img->attr['src']);
+	$title = explode('第',$img->attr['alt']);
+	$title = trim(eregi_replace("[^0-9]","",$title[0]));
+}
+
+$count = 5;
+$end = false;
+for ($i = $num*$count; $i < $num*$count+$count; $i++) {
+	if (!isset($imgsUrl[$i])) {
+		$end = true;
+		break;
+	}
+	GrabImage($imgsUrl[$i], ($i<10 ? "0$i.jpg" : "$i.jpg"), "./$title/");
+}
+
+$url = 'http://' . $_SERVER['HTTP_HOST'].$_SERVER["REQUEST_URI"];
+$url = explode('&', $url);
+$url = $url[0] . '&n=' . (++$num);
+
+if (!$end) {
+	$total = $count * $num;
+	echo "<html><head><head><body><meta http-equiv=\"refresh\" content=\"1;url=$url\">已抓取 $total 页</body></html>";
+} else {
+	echo 'end';
+}
+
+
+	
+function GrabImage($url,$filename="", $filedir="./") {
+   if($url=="") return false;
+
+	
+   if (!file_exists($filedir)) {  
+   	mkdir($filedir, 0777);   
+   }
+   
+   ob_start();
+   readfile($url);
+   $img = ob_get_contents();
+   ob_end_clean();
+   $size = strlen($img);
+
+   $fp2=@fopen($filedir.$filename, "a");
+   fwrite($fp2,$img);
+   fclose($fp2);
+
+   return $filedir.$filename;
+} 
+?>
---
+++ trunk/ndzk/index.php
@@ -0,0 +1,31 @@
+﻿<html>
+<head><title>南都周刊</title></head>
+<body>
+<?php
+require("simple_html_dom.php");
+if (isset($_GET['p'])) {
+	$page = (int) $_GET['p'];
+} else {
+	$page = 1;
+}
+$html = file_get_html ( "http://www.nbweekly.com/Print/Page/Default.aspx?Page=$page" );
+$books = $html->find("#MyDataList li");
+$bookArr = array();
+foreach ( $books as $book ) {
+	$a = $book->find('a');
+	$bookArr[] = array('href' => $a[0]->attr['href'], 'title' => $a[0]->attr['title'], 'src' => $a[0]->children[0]->attr['src']);
+}
+echo '<ul>';
+foreach ($bookArr as $book) {
+	$url = 'http://www.nbweekly.com' . $book['href'];
+	echo '<li><a href="collect.php?url=' . $url . '"><img src="http://www.nbweekly.com' . $book['src'] .'" /></a>';
+	
+}
+echo '</ul>';
+if ($page != 1) {
+	echo '<a href="index.php?p=' . ($page-1) . '">上一页</a>';
+}
+	echo '<a href="index.php?p=' . ($page+1) . '">下一页</a>';
+?>
+</body>
+</html>
