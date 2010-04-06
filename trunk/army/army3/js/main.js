ac6b7a0b3e7348fda00a64db4601685f5088a3f4
bang590 <bang590@9944b59e-f2dc-11de-8b01-fd14ac940ccb>
2010-04-06 16:26:37 +0000

    git-svn-id: http://bangswork.googlecode.com/svn@36 9944b59e-f2dc-11de-8b01-fd14ac940ccb

--- trunk/army/army3/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$("#restartBtn").click(function(){
-		var board = $("#board");
-		Army.init.startInit();
-		Army.init.createSpace(board);
-		Army.init.createPieces(board);
-	})
-});
-
